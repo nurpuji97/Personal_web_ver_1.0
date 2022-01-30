55e0d3fb5661db5ea44c2a875f6c42fbab1562b2
DESKTOP-VHNF39T\nurpu <nurpujiyanto1997@gmail.com>
2022-01-30 14:18:05 +0700

    Page Home Selesai

--- main.js
+++ main.js
@@ -1,6 +1,46 @@
+// burger menu
 const btnBurger = document.querySelector('.Burger_menu');
 const ListMenu = document.querySelector('.nav__menu');
 
 btnBurger.addEventListener('click', ()=>{
     ListMenu.classList.toggle('active')
+});
+
+// swiper js
+var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 1,
+    spaceBetween: 10,
+    loop: true,
+    loopFillGroupWithBlank: true,
+    cssMode: true,
+    autoplay: {
+      delay: 8000,
+      disableOnInteraction: false,
+    },
+    pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+    },
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+    breakpoints: {
+    400: {
+        slidesPerView: 1,
+        spaceBetween: 0,
+    },
+    640: {
+        slidesPerView: 1,
+        spaceBetween: 10,
+    },
+    768: {
+        slidesPerView: 2,
+        spaceBetween: 20,
+    },
+    1024: {
+        slidesPerView: 3,
+        spaceBetween: 30,
+    },
+    },
 });
